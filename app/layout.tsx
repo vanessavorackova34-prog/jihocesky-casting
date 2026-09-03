f09d051233489479eb388403241c816915b496d2
vanessavorackova34-prog <vanessavorackova34@gmail.com>
2026-09-04 00:37:46 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Jihočeský casting",
+  description: "Registrace na casting",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="cs">
+      <body>{children}</body>
+    </html>
+  );
+}
